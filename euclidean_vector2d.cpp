7729989175b7e0bcb60799580a61c6bf8fba97fe
GELIOS-19 <64653918+GELIOS-19@users.noreply.github.com>
2021-11-29 16:25:28 -0500

    Update euclidean_vector2d.cpp

--- euclidean_vector2d.cpp
+++ euclidean_vector2d.cpp
@@ -1,64 +1,119 @@
-#include <iostream>
-#include <vector>
-#include <map>
+#ifndef CHESS_ENGINE_HPP
+#define CHESS_ENGINE_HPP
 
-#define PI 3.141592
+/////////////////////////////////////////////////////////////
+/// Headers
+/////////////////////////////////////////////////////////////
+#include "../Shared/Libraries.hpp"
 
-class c_direction
-{
+namespace chess {
 
-public:
-	
-	static const c_direction null;
-	static const c_direction north;
-	static const c_direction south;
-	static const c_direction east;
-	static const c_direction west;
+////////////////////////////////////////////////////////////
+/// \brief Contains all OpenGL functionality
+///
+////////////////////////////////////////////////////////////
+namespace engine {
 
-	int x_prefix;
-	int y_prefix;
+namespace priv {
 
-	c_direction(int x_prefix, int y_prefix)
-	{
-		this->x_prefix = x_prefix;
-		this->y_prefix = y_prefix;
-	}
-
-	bool operator ==(const c_direction& subject) const
-	{
-		return x_prefix == subject.x_prefix && y_prefix == subject.y_prefix;
-	}
-};
-
-c_direction const c_direction::null = c_direction(0, 0);
-c_direction const c_direction::north = c_direction(0, 1);
-c_direction const c_direction::south = c_direction(0, -1);
-c_direction const c_direction::east = c_direction(1, 0);
-c_direction const c_direction::west = c_direction(-1, 0);
-
-struct c_vector_component
-{
-	double magnitude;
-	c_direction direction;
-
-	double represent_as_double() const
-	{
-		if (direction == c_direction::north || direction == c_direction::south)
-			return magnitude * direction.y_prefix;
-		else if (direction == c_direction::east || direction == c_direction::west)
-			return magnitude * direction.x_prefix;
-		return 0;
-	}
-
-	static c_vector_component represent_as_vector_component(const c_vector_component& subject)
-	{
-
-	}
-};
-
-int main(int argc, const char* argv[])
-{
-	std::cout << "Hello World" << std::endl;
-	return 0;
+////////////////////////////////////////////////////////////
+/// \brief Callback function for the engine window
+///
+/// \param window Window the callback function is called for
+/// \param frameBufferWidth Width of the frame buffer
+/// \param frameBufferHeight Height of the frame buffer
+/// 
+////////////////////////////////////////////////////////////
+static void frameBufferSizeCallback(GLFWwindow* window, int frameBufferWidth, int frameBufferHeight) {
+  glViewport(0, 0, frameBufferWidth, frameBufferHeight);
 }
-s
+
+} // namespace priv
+
+////////////////////////////////////////////////////////////
+/// \brief Creates a window with GLFW and GLEW and sets it to engineWindow
+///
+/// Requires glfwInit() to be called prior to calling this method.
+///
+/// \throws GLEW initialization error
+///
+/// \param windowWidth Width of the window
+/// \param windowHeight Height of the window
+/// \param windowTitle Title of the window
+/// \param frameBufferWidth Width of the frame buffer
+/// \param frameBufferHeight Height of the frame buffer
+/// \param makeResizable Indicates if the window should be made resizable
+/// 
+////////////////////////////////////////////////////////////
+static GLFWwindow* createWindow(const int windowWidth,
+                                const int windowHeight,
+                                const std::string windowTitle,
+                                int frameBufferWidth,
+                                int frameBufferHeight,
+                                const bool makeResizable) {
+  // Create Window
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_RESIZABLE, makeResizable ? GL_TRUE : GL_FALSE);
+
+  GLFWwindow* window = glfwCreateWindow(windowWidth,
+                                        windowHeight,
+                                        windowTitle.c_str(),
+                                        nullptr,
+                                        nullptr);
+
+  if (makeResizable) {
+    glfwSetFramebufferSizeCallback(window, priv::frameBufferSizeCallback);
+  } else {
+    glfwGetFramebufferSize(window, &frameBufferWidth, &frameBufferHeight);
+    glViewport(0, 0, frameBufferWidth, frameBufferHeight);
+  }
+  
+  glfwMakeContextCurrent(window);
+
+  // Initialize GLEW
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK) {
+    glfwTerminate();
+    throw "Error :: Engine.cpp :: GLEW initialization failed";
+  }
+
+  return window;
+}
+
+////////////////////////////////////////////////////////////
+/// \brief Calls methods preliminary to drawing
+///
+/// \param window Window to affect
+///
+////////////////////////////////////////////////////////////
+static void beginWindowDraw(GLFWwindow* window) {
+  // Make the window current context if it is not already
+  if (window != glfwGetCurrentContext())
+    glfwMakeContextCurrent(window);
+  
+  glClearColor(0.f, 0.f, 0.f, 1.f); // The color used here does not matter
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+}
+
+////////////////////////////////////////////////////////////
+/// \brief Calls methods after drawing
+///
+/// \param window Window to affect
+///
+////////////////////////////////////////////////////////////
+static void endWindowDraw(GLFWwindow* window) {
+  // Make the window current context if it is not already
+  if (window != glfwGetCurrentContext())
+    glfwMakeContextCurrent(window);
+  
+  glfwSwapBuffers(window);
+  glFlush();
+}
+
+} // namespace engine
+} // namespace chess
+
+
+#endif // #ifndef CHESS_ENGINE_HPP
