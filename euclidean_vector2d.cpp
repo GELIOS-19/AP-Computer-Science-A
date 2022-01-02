0aa9ebbc8d76d78fc6a203afdb94e53f7df0b823
GELIOS-19 <64653918+GELIOS-19@users.noreply.github.com>
2022-01-02 17:23:57 -0500

    Update euclidean_vector2d.cpp

--- euclidean_vector2d.cpp
+++ euclidean_vector2d.cpp
@@ -1,172 +1,42 @@
-#ifndef CHESS_ENGINE_HPP
-#define CHESS_ENGINE_HPP
+//
+// Created by Slend on 1/2/2022.
+//
 
-/////////////////////////////////////////////////////////////
-/// Headers
-/////////////////////////////////////////////////////////////
-#include "../Shared/Libraries.hpp"
+#include "Board.h"
 
+namespace core::models {
 
-namespace chess {
+Board::Board() : sideLength_(3) {
+  // initialize total squares
+  this->totalSquares_ = 9;
 
-////////////////////////////////////////////////////////////
-/// \brief Contains all OpenGL functionality
-///
-////////////////////////////////////////////////////////////
-namespace engine {
+  // initialize board squares
+  for (uint16_t index = 0; index < this->totalSquares_; index++)
+    this->boardSquares_[index] = new BoardSquare(index, false, nullptr);
 
-    ////////////////////////////////////////////////////////////
-    /// \brief Contains private items 
-    ///
-    ////////////////////////////////////////////////////////////
-    namespace priv {
+  // initialize winning indices sets
+  this->winningIndicesSets_ = {{0, 1, 2},
+                               {3, 4, 5},
+                               {6, 7, 8},
+                               {0, 3, 6},
+                               {1, 4, 7},
+                               {2, 5, 8},
+                               {0, 4, 8},
+                               {2, 4, 6}};
+}
 
-        ////////////////////////////////////////////////////////////
-        /// \brief Contains functionality dealing with windows 
-        ///
-        ////////////////////////////////////////////////////////////
-        namespace window {
+Board::Board(uint16_t sideLength) : sideLength_(sideLength) {
+  // initialize total squares
+  this->totalSquares_ = static_cast<uint16_t>(pow(this->sideLength_, 2));
 
-            ////////////////////////////////////////////////////////////
-            /// \brief Callback function for the engine window
-            ///
-            /// \param window Window the callback function is called for
-            /// \param frameBufferWidth Width of the frame buffer
-            /// \param frameBufferHeight Height of the frame buffer
-            ///
-            ////////////////////////////////////////////////////////////
-            static void frameBufferSizeCallback(GLFWwindow* window, int frameBufferWidth, int frameBufferHeight)
-            {
-                glViewport(0, 0, frameBufferWidth, frameBufferHeight);
-            }
+  // initialize board squares
+  for (uint16_t index = 0; index < this->totalSquares_; index++)
+    this->boardSquares_[index] = new BoardSquare(index, false, nullptr);
 
-        } // namespace window
+  // initialize winning indices sets
+  std::vector<std::vector<uint16_t>> horizontalIndicesSet;
+  std::vector<std::vector<uint16_t>> verticalIndicesSet;
+  std::vector<std::vector<uint16_t>> diagonalIndicesSet;
+}
 
-    } // namespace priv
-
-    ////////////////////////////////////////////////////////////
-    /// \brief Contains functionality dealing with windows 
-    ///
-    ////////////////////////////////////////////////////////////
-    namespace window {
-
-        ////////////////////////////////////////////////////////////
-        /// \brief Creates a window with GLFW and GLEW and sets it to engineWindow
-        ///
-        /// Requires glfwInit() to be called prior to calling this method.
-        ///
-        /// \throws GLEW initialization error
-        ///
-        /// \param windowWidth Width of the window
-        /// \param windowHeight Height of the window
-        /// \param windowTitle Title of the window
-        /// \param frameBufferWidth Width of the frame buffer
-        /// \param frameBufferHeight Height of the frame buffer
-        /// \param makeResizable Indicates if the window should be made resizable
-        ///
-        ////////////////////////////////////////////////////////////
-        static GLFWwindow* createWindow(
-            const int windowWidth,
-            const int windowHeight,
-            const std::string windowTitle,
-            int frameBufferWidth,
-            int frameBufferHeight,
-            const bool makeResizable)
-        {
-            // Set window hints
-            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-            glfwWindowHint(GLFW_RESIZABLE, makeResizable ? GL_TRUE : GL_FALSE);
-
-            // Create window
-            GLFWwindow* window = glfwCreateWindow(
-                windowWidth,
-                windowHeight,
-                windowTitle.c_str(),
-                nullptr,
-                nullptr);
-
-            // Set frame buffer properties
-            if (makeResizable) {
-                glfwSetFramebufferSizeCallback(window, priv::window::frameBufferSizeCallback);
-            } else {
-                glfwGetFramebufferSize(window, &frameBufferWidth, &frameBufferHeight);
-                glViewport(0, 0, frameBufferWidth, frameBufferHeight);
-            }
-
-            // Make the window current context
-            glfwMakeContextCurrent(window);
-
-            // Initialize GLEW
-            glewExperimental = GL_TRUE;
-            if (glewInit() != GLEW_OK) {
-                glfwTerminate();
-                throw "Error :: Engine.cpp :: GLEW initialization failed";
-            }
-
-            return window;
-        }
-
-        ////////////////////////////////////////////////////////////
-        /// \brief Updates window inputs
-        ///
-        /// \param window Window to affect
-        ///
-        ////////////////////////////////////////////////////////////
-        static void updateWindowInputs(GLFWwindow* window)
-        {
-            // Make the window current context if it is not already
-            if (window != glfwGetCurrentContext())
-                glfwMakeContextCurrent(window);
-
-            // Poll events
-            glfwPollEvents();
-        }
-
-        ////////////////////////////////////////////////////////////
-        /// \brief Calls methods preliminary to drawing
-        ///
-        /// \param window Window to affect
-        ///
-        ////////////////////////////////////////////////////////////
-        static void beginWindowDraw(GLFWwindow* window)
-        {
-            // Make the window current context if it is not already
-            if (window != glfwGetCurrentContext())
-                glfwMakeContextCurrent(window);
-
-            // Clear with color
-            glClearColor(0.f, 0.f, 0.f, 1.f); // The color used here does not matter
-
-            // Clear buffers
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-        }
-
-        ////////////////////////////////////////////////////////////
-        /// \brief Calls methods after drawing
-        ///
-        /// \param window Window to affect
-        ///
-        ////////////////////////////////////////////////////////////
-        static void endWindowDraw(GLFWwindow* window)
-        {
-            // Make the window current context if it is not already
-            if (window != glfwGetCurrentContext())
-                glfwMakeContextCurrent(window);
-
-            // Swap buffers
-            glfwSwapBuffers(window);
-
-            // Flush
-            glFlush();
-        }
-
-    } // namespace window
-
-} // namespace engine
-
-} // namespace chess
-
-
-#endif // #ifndef CHESS_ENGINE_HPP
+} // namespace core::models
