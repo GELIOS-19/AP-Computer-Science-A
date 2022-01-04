b0fc42737d783882cb2b59810c106aa518f0712d
GELIOS-19 <64653918+GELIOS-19@users.noreply.github.com>
2022-01-03 20:18:01 -0500

    Update euclidean_vector2d.cpp

--- euclidean_vector2d.cpp
+++ euclidean_vector2d.cpp
@@ -1,112 +1,152 @@
 //
 // Created by Slend on 1/2/2022.
 //
-
 #include "Board.h"
 
-namespace core::models {
-
-Board::Board()
-	: sideLength_(3), totalSquares_(9) {
-	// initialize board squares
-	for (uint16_t index = 0; index < this->totalSquares_; index++) {
-		this->boardSquares_[index] = new BoardSquare(index, false, nullptr);
-	}
-
-	// initialize winning indices sets for 3x3 board
-	this->winningIndicesSets_ = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // horizontal indices lists
-								 {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // vertical indices lists
-								 {0, 4, 8}, {2, 4, 6}}; // diagonal indices lists
-}
-
-Board::Board(uint16_t sideLength)
-	: sideLength_(sideLength), totalSquares_(static_cast<uint16_t>(pow(this->sideLength_, 2))) {
-
-	// initialize board squares
-	for (uint16_t index = 0; index < this->totalSquares_; index++) {
-		this->boardSquares_[index] = new BoardSquare(index, false, nullptr);
-	}
-
-	// initialize winning indices sets
-	std::vector<std::vector<uint16_t>> horizontalIndicesSets; // represents list of lists of horizontal indices
-	std::vector<std::vector<uint16_t>> verticalIndicesSets; // represents list of lists of vertical indices
-	std::vector<std::vector<uint16_t>> diagonalIndicesSets; // represents list of lists of diagonal indices
-
-	// reserve memory
-	horizontalIndicesSets.reserve(this->sideLength_);
-	verticalIndicesSets.reserve(this->sideLength_);
-	diagonalIndicesSets.reserve(this->sideLength_);
-
-	// get all lists of horizontal indices
-	std::vector<uint16_t> horizontalIndicesSet;
-
-	for (uint16_t index = 0; index < this->totalSquares_; index++) {
-		horizontalIndicesSet.push_back(index);
-		if (horizontalIndicesSet.size() == this->sideLength_) {
-			horizontalIndicesSets.push_back(horizontalIndicesSet);
-			horizontalIndicesSet.clear();
-		}
-	}
-
-	// get all lists of vertical indices
-	std::vector<uint16_t> verticalIndicesSet;
-
-	for (uint16_t index = 0; index < this->sideLength_; index++) {
-		for (const auto &horizontalIndicesSet_ : horizontalIndicesSets) {
-			verticalIndicesSet.push_back(horizontalIndicesSet_[index]);
-			if (verticalIndicesSet.size() == this->sideLength_) {
-				verticalIndicesSets.push_back(verticalIndicesSet);
-				verticalIndicesSet.clear();
+namespace core::models
+{
+	std::vector<std::vector < uint16_t>> Board::initWinningIndicesSets_(uint16_t sideLength, uint16_t totalSquares)
+	{
+		std::vector<std::vector < uint16_t>> hSets;	// represents list of lists of horizontal indices
+		hSets.reserve(sideLength);
+		
+		// get all lists of horizontal indices
+		std::vector<uint16_t> hSet;
+		
+		for (uint16_t index = 0; index < totalSquares; index++)
+		{
+			hSet.push_back(index);
+			if (hSet.size() == sideLength)
+			{
+				hSets.push_back(hSet);
+				hSet.clear();
 			}
 		}
+
+		std::vector<std::vector < uint16_t>> vSets;	// represents list of lists of vertical indices
+		vSets.reserve(sideLength);
+		
+		// get all lists of vertical indices
+		std::vector<uint16_t> vSet;
+		
+		for (uint16_t index = 0; index < sideLength; index++)
+			for (const auto &hSetTemp: hSets)
+			{
+				vSet.push_back(hSetTemp[index]);
+				if (vSet.size() == sideLength)
+				{
+					vSets.push_back(vSet);
+					vSet.clear();
+				}
+			}
+
+		std::vector<std::vector < uint16_t>> dSets;	// represents list of lists of diagonal indices
+		dSets.reserve(sideLength);
+		
+		// get all lists of diagonal indices
+		std::vector<uint16_t> dSet;
+		
+		for (uint64_t index = 0; index < hSets.size(); index++)
+		{
+			auto &hSetTemp = hSets[index];
+			dSet.push_back(hSetTemp[index]);
+		}
+
+		dSets.push_back(dSet);
+		dSet.clear();
+
+		for (uint64_t index = 0; index < hSets.size(); index++)
+		{
+			auto &hSetTemp = hSets[index];
+			dSet.push_back(hSetTemp[hSet.size() - (index + 1)]);
+		}
+
+		dSets.push_back(dSet);
+
+		std::vector<std::vector < uint16_t>> wSets;	// represents list of lists of all indices
+		wSets.reserve(hSets.size() + vSets.size() + dSets.size());
+		
+		// add all the lists of indices to the winning indices sets
+		for (auto &hSetTemp : hSets) wSets.push_back(hSetTemp);
+		for (auto &vSetTemp : vSets) wSets.push_back(vSetTemp);
+		for (auto &dSetTemp : dSets) wSets.push_back(dSetTemp);
+
+		return wSets;
 	}
 
-	// get all lists of diagonal indices
-	std::vector<uint16_t> diagonalIndicesSet;
-
-	for (uint64_t index = 0; index < horizontalIndicesSets.size(); index++) {
-		auto &horizontalIndicesSet_ = horizontalIndicesSets[index];
-		diagonalIndicesSet.push_back(horizontalIndicesSet_[index]);
+	Board::Board(): sideLength_(3), totalSquares_(9)
+	{
+		this->boardSquares_.reserve(9);
+		this->boardSquares_[0] = new BoardSquare(0, false, nullptr);
+		this->boardSquares_[1] = new BoardSquare(1, false, nullptr);
+		this->boardSquares_[2] = new BoardSquare(2, false, nullptr);
+		this->boardSquares_[3] = new BoardSquare(3, false, nullptr);
+		this->boardSquares_[4] = new BoardSquare(4, false, nullptr);
+		this->boardSquares_[5] = new BoardSquare(5, false, nullptr);
+		this->boardSquares_[6] = new BoardSquare(6, false, nullptr);
+		this->boardSquares_[7] = new BoardSquare(7, false, nullptr);
+		this->boardSquares_[8] = new BoardSquare(8, false, nullptr);
+		this->winningIndicesSets_ = {
+		    { 0, 1, 2 },
+			{ 3, 4, 5 },
+			{ 6, 7, 8 },
+			//
+			{ 0, 3, 6 },
+			{ 1, 4, 7 },
+			{ 2, 5, 8 },
+			//
+			{ 0, 4, 8 },
+			{ 2, 4, 6 }
+		};
 	}
 
-	diagonalIndicesSets.push_back(diagonalIndicesSet);
-	diagonalIndicesSet.clear();
-
-	for (uint64_t index = 0; index < horizontalIndicesSets.size(); index++) {
-		auto &horizontalIndicesSet_ = horizontalIndicesSets[index];
-		diagonalIndicesSet.push_back(horizontalIndicesSet_[horizontalIndicesSet.size() - (index + 1)]);
+	Board::Board(uint16_t sideLength) : sideLength_(sideLength), totalSquares_(static_cast<uint16_t> (pow(this->sideLength_, 2)))
+	{
+		for (uint16_t index = 0; index < this->totalSquares_; index++)
+			this->boardSquares_[index] = new BoardSquare(index, false, nullptr);
+		this->winningIndicesSets_ = initWinningIndicesSets_(this->sideLength_, this->totalSquares_);
 	}
 
-	diagonalIndicesSets.push_back(diagonalIndicesSet);
-
-	// add all the lists of indices to the winning indices sets
-	for (auto &horizontalIndicesSet_ : horizontalIndicesSets) {
-		this->winningIndicesSets_.push_back(horizontalIndicesSet_);
+	Board::~Board()
+	{
+		std::vector<BoardSquare*>::iterator iter;
+		for (iter = this->boardSquares_.begin(); iter != this->boardSquares_.end(); iter++)
+		{
+			delete * iter;
+			iter = this->boardSquares_.erase(iter);
+		}
 	}
 
-	for (auto &verticalIndicesSet_ : verticalIndicesSets) {
-		this->winningIndicesSets_.push_back(verticalIndicesSet_);
+	std::vector<BoardSquare*> Board::getBoardSquares() const
+	{
+		return this->boardSquares_;
 	}
 
-	for (auto &diagonalIndicesSet_ : diagonalIndicesSets) {
-		this->winningIndicesSets_.push_back(diagonalIndicesSet_);
+	std::vector<std::vector < uint16_t>> Board::getWinningIndicesSets() const
+	{
+		return this->winningIndicesSets_;
 	}
-}
 
-Board::~Board() {
-	std::vector<BoardSquare *>::iterator iter;
-	for (iter = this->boardSquares_.begin(); iter != this->boardSquares_.end(); iter++) {
-		delete *iter;
-		iter = this->boardSquares_.erase(iter);
+	uint16_t Board::getSideLength() const
+	{
+		return this->sideLength_;
 	}
-}
 
-std::vector<BoardSquare *> Board::getBoardSquares() const {
-	return this->boardSquares_;
-}
+	uint16_t Board::getTotalSquares() const
+	{
+		return this->totalSquares_;
+	}
 
-std::vector<std::vector<uint16_t>> Board::getWinningIndicesSets() const {
-	return this->winningIndicesSets_;
-}
-
-} // namespace core::models
+	std::string Board::toJSONString() const
+	{
+		std::stringstream ss;
+		ss << "{"
+			  "\"sideLength\": " << this->sideLength_ << ","
+			  "\"totalSquares\": " << this->totalSquares_ << ","
+			  "\"boardSquares\": {";
+		for (const BoardSquare *bs: boardSquares_)
+			ss << "\"BoardSquare@index=" << bs->getIndex() << "\": " << bs->toJSONString() << ",";
+		ss << "}";
+		return ss.str();
+	}
+}	// namespace core::models
