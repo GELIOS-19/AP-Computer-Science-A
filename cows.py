51ad53900d24cf736cfd239d357df1c354e75310
GELIOS-19 <64653918+GELIOS-19@users.noreply.github.com>
2022-03-31 10:08:33 -0400

    Update cows.py

--- cows.py
+++ cows.py
@@ -16,14 +16,15 @@ for cnst in cnsts:
     cows_adj[cows.index(cnst[1])].append(cnst[0])
 
 # find all the cows that less than 2 constraints
-one_or_none_cnsts = []
+flex_cows = []
 for cow_adj in cows_adj:
     if len(cow_adj) < 2:
-        one_or_none_cnsts.append(cows_adj.index(cow_adj))
-one_or_none_cnsts.sort()
+        flex_cows.append(cows_adj.index(cow_adj))
+flex_cows.sort()
 
-# add the first and last cow to the answer and remove them from the one or none constraints list
-ans[0] = one_or_none_cnsts[0]
-one_or_none_cnsts.pop(0)
-ans.append(one_or_none_cnsts[-1])
-one_or_none_cnsts.pop()
+# add the first and last cow to the answer and remove them from the one or none 
+# constraints list
+ans[0] = flex_cows[0]
+flex_cows.pop(0)
+ans.append(flex_cows[-1])
+flex_cows.pop()
