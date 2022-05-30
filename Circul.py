11d5288e66ae95274a9ff717d6194b193d23a948
GELIOS-19 <64653918+GELIOS-19@users.noreply.github.com>
2022-05-30 19:53:08 -0400

    Update Circul.py

--- Circul.py
+++ Circul.py
@@ -12,51 +12,44 @@ mass_vs_spring_length = {
 
 natural_spring_length = 6.3
 
-ACCEL_DUE_TO_GRAV = 9.81
+ACCELERATION_DUE_TO_GRAVITY = 9.81
 
 
 def main():
     # Format data
-    y = np.array(
-        [(m / 1000) * ACCEL_DUE_TO_GRAV for m in mass_vs_spring_length.keys()]
-    )
-    x = np.array(
-        [
-            (l / 100) - (natural_spring_length / 100)
-            for l in mass_vs_spring_length.values()
-        ]
-    )
+    y_axis = np.array([(mass / 1000) * ACCELERATION_DUE_TO_GRAVITY
+                       for mass in mass_vs_spring_length.keys()])
+    x_axis = np.array([(spring_length / 100) - (natural_spring_length / 100)
+                       for spring_length in mass_vs_spring_length.values()])
 
     # create the plot
-    plot = plt.figure(dpi=1200, figsize=(10, 5))
+    plot = plt.figure(dpi=284, figsize=(10, 5))
     subplot = plot.add_subplot(1, 1, 1)
-    subplot.scatter(x, y)
+    subplot.scatter(x_axis, y_axis)
 
     # label the axes
+    subplot.set_title("Force vs. Displacement")
     subplot.set_xlabel("Displacement (m)")
     subplot.set_ylabel("Force (N)")
-    subplot.set_title("Force vs. Displacement")
 
     # label each point on the plot with its coordinates
-    anns = []
-    for x_coord, y_coord in zip(x, y):
-        anns.append(
-            subplot.annotate(
-                f"({x_coord:.2} m, {y_coord:.2} N)",
-                xy=(x_coord + (3 / 1000), y_coord - (25 / 1000))
-            )
-        )
+    annotations = []
+    for x_coordinate, y_coordinate in zip(x_axis, y_axis):
+        annotations.append(
+            subplot.annotate(f"({x_coordinate:.2} m, {y_coordinate:.2} N)",
+                             xy=(x_coordinate + (3 / 1000),
+                                 y_coordinate - (25 / 1000))))
     # save without trend line
     plt.savefig("without_trend_line.png")
 
     # remove annotations
-    for ann in anns:
-        ann.remove()
+    for annotation in annotations:
+        annotation.remove()
 
     # create best fit line
-    m, b = np.polyfit(x, y, 1)
-    print(m, b)
-    subplot.plot(x, m * x + b, '-', color='red')
+    slope, y_intercept = np.polyfit(x_axis, y_axis, 1)
+    print(slope, y_intercept)
+    subplot.plot(x_axis, slope * x_axis + y_intercept, '-', color='red')
     # save with trend line
     plt.savefig("with_trend_line.png")
 
